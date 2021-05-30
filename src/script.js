3b1f1664f63eccace613b0c2b35ccfc18814808a
Jenny Janssen <51443405+jennyj1414@users.noreply.github.com>
2021-05-30 13:48:58 +0200

    Added js script to show current weather data
    axios issue persists

--- src/script.js
+++ src/script.js
@@ -1,5 +1,15 @@
 function displayTemperature(response) {
-  console.log(response.data);
+  let currentTemperature = document.querySelector(".currentTemperature");
+  let city = document.querySelector(".city");
+  let description = document.querySelector(".description");
+  let currentHumidity = document.querySelector(".current-humidity");
+  let currentWind = document.querySelector(".current-wind");
+
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  currentHumidity.innerHTML = response.data.main.humidity;
+  currentWind.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "4f0c3132958148b431199806e8e0d604";
 
