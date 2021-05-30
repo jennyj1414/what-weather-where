a4622e8bc9055c30918c1d72f6dcd981a1a4c5d0
Jenny Janssen <51443405+jennyj1414@users.noreply.github.com>
2021-05-30 17:38:11 +0200

    added unit conversion

--- src/script.js
+++ src/script.js
@@ -31,6 +31,8 @@ function displayTemperature(response) {
   let currentDate = document.querySelector(".current-date-value");
   let currentWeatherIcon = document.querySelector(".current-icon");
 
+  celsiusTemperature = response.data.main.temp;
+
   city.innerHTML = response.data.name;
   countryCode.innerHTML = response.data.sys.country;
   currentTemperature.innerHTML = Math.round(response.data.main.temp);
@@ -60,7 +62,32 @@ function handleSubmit(event) {
   search(searchInput.value);
 }
 
-search("New York");
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let currentTemperature = document.querySelector(".current-temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  currentTemperature.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let currentTemperature = document.querySelector(".current-temperature");
+  currentTemperature.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector(".search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
