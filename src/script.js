76999d7908118a8a1a70f461f33937836da23c42
Jenny Janssen <51443405+jennyj1414@users.noreply.github.com>
2021-05-30 14:06:36 +0200

    added js script to update time

--- src/script.js
+++ src/script.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours > 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes > 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let currentTemperature = document.querySelector(".currentTemperature");
   let city = document.querySelector(".city");
@@ -10,6 +33,8 @@ function displayTemperature(response) {
   description.innerHTML = response.data.weather[0].description;
   currentHumidity.innerHTML = response.data.main.humidity;
   currentWind.innerHTML = Math.round(response.data.wind.speed);
+
+  currentDate.innerHTML = formatDate(repsonse.data.dt * 1000);
 }
 let apiKey = "4f0c3132958148b431199806e8e0d604";
 
