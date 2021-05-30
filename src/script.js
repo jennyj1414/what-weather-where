f75f66fdbcf2ca7382bcfb2b6f2ddbf33788a1db
Jenny Janssen <51443405+jennyj1414@users.noreply.github.com>
2021-05-30 14:33:05 +0200

    created js code to update weather icon
    axios issue persists

--- src/script.js
+++ src/script.js
@@ -27,6 +27,7 @@ function displayTemperature(response) {
   let description = document.querySelector(".description");
   let currentHumidity = document.querySelector(".current-humidity");
   let currentWind = document.querySelector(".current-wind");
+  let currentWeatherIcon = doxument.querySelector(".current-icon");
 
   currentTemperature.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
@@ -35,9 +36,18 @@ function displayTemperature(response) {
   currentWind.innerHTML = Math.round(response.data.wind.speed);
 
   currentDate.innerHTML = formatDate(repsonse.data.dt * 1000);
+
+  currentWeatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  currentWeatherIcon.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "4f0c3132958148b431199806e8e0d604";
 
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units-metric`;
+let city = "Berlin";
+
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units-metric`;
 
 axios.get(apiUrl).then(displayTemperature);
