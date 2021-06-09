34edba4eb4577dcac1e5ecdaf64bbed4b9056df7
Jenny Janssen <51443405+jennyj1414@users.noreply.github.com>
2021-06-09 21:15:51 +0200

    start forecast with next day (not today)

--- src/script.js
+++ src/script.js
@@ -44,7 +44,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecastDaily.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index > 0 && index < 6) {
       forecastHTML =
         forecastHTML +
         `
